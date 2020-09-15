e1862c14054848a961a063de2912222b3b3e3668
tarlepp <tarmo.leppanen@protacon.com>
2020-09-15 19:43:19 +0300

    Initial commit

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types = 1);
+
+namespace App\Controller;
+
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Panther\Client;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController
+{
+    /**
+     * @Route(
+     *      path="/",
+     *      name="index",
+     *  )
+     */
+    public function __invoke(): Response
+    {
+        $client = Client::createChromeClient();
+
+        $client->request('GET', 'https://www.dia.es/compra-online/');
+
+        $crawler = $client->waitFor('#nav-submenu-container');
+
+        $crawler->filter('#nav-submenu-container > li > a')->reduce(function (Crawler $node) {
+            dump($node->html());
+        });
+
+        $client->close();
+
+        dd('jee');
+    }
+}
